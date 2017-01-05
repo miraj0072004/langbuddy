90f24b97921a2c355ffddb9a91299f0cf0e5c9c5
miraj <miraj0072004>
2017-01-05 09:59:00 +0530

    Added default task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,20 @@
 var gulp=require ('gulp'),
-    gutil=require('gulp-util');
+    gutil=require('gulp-util'),
+    concat=require('gulp-concat'),
+    browserify=require('gulp-browserify');
+    
+
+var jsSources=['components/scripts/temp.js'];
 
 gulp.task('log',function(){    
     gutil.log("Workflows are awesome");
 });
+
+gulp.task('js',function(){
+    gulp.src(jsSources)
+    .pipe(concat('script.js'))
+    .pipe(browserify())
+    .pipe(gulp.dest('builds/development/js'))
+});
+
+gulp.task('default',['js']);
